15b287597867b3c5e05a3e2d7eb33f1609c53579
Adarsh Dhar <dharadarsh0@gmail.com>
2025-10-31 08:05:52 +0530

    1

--- lib/blockchain-utils.ts
+++ lib/blockchain-utils.ts
@@ -1,7 +1,7 @@
 import { Connection, PublicKey } from '@solana/web3.js';
 import { Program, AnchorProvider } from '@coral-xyz/anchor';
 import * as anchor from '@coral-xyz/anchor';
-import NEWS_PLATFORM_IDL from '../contract/target/idl/news_platform.json';
+import NEWS_PLATFORM_IDL from '../app/news_platform.json';
 
 // Get connection and program
 function getConnection(): Connection {
